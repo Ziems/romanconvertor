b3466f320aa6b70fb5c370d69fe229731f5830df
Noah Ziems <nmziems@bsu.edu>
2020-09-03 13:40:11 -0400

    Implement code for toRoman and fromRoman

--- romanconvertor.rb
+++ romanconvertor.rb
@@ -1,9 +1,61 @@
 def fromRoman(romanNumber)
-    # Replace the following line with the actual code!
-    raise NotImplementedError
+    rom = romanNumber.dup
+    @data = [
+        ["M"  , 1000],
+        ["CM" , 900],
+        ["D"  , 500],
+        ["CD" , 400],
+        ["C"  , 100],
+        ["XC" ,  90],
+        ["L"  ,  50],
+        ["XL" ,  40],
+        ["X"  ,  10],
+        ["IX" ,   9],
+        ["V"  ,   5],
+        ["IV" ,   4],
+        ["I"  ,   1]
+    ]   
+    if (/^[IVXLCDM]*$/.match?(rom) == false)
+        raise TypeError
+    end
+    ara = 0
+    for key, value in @data
+        while rom.index(key) == 0
+            ara += value
+            rom.slice!(key)
+        end
+    end
+    return ara
 end
 
 def toRoman(arabicNumber)
-    # Replace the following line with the actual code!
-    raise NotImplementedError
+    ara = arabicNumber.dup
+    @data = [
+        ["M"  , 1000],
+        ["CM" , 900],
+        ["D"  , 500],
+        ["CD" , 400],
+        ["C"  , 100],
+        ["XC" ,  90],
+        ["L"  ,  50],
+        ["XL" ,  40],
+        ["X"  ,  10],
+        ["IX" ,   9],
+        ["V"  ,   5],
+        ["IV" ,   4],
+        ["I"  ,   1],
+    ]  
+
+    if (ara > 3999 || ara < 1)
+        raise RangeError
+    end
+    roman = ""
+
+    @data.each do |pair|
+        letter = pair[0]
+        value = pair[1]
+        roman += letter*(ara / value)
+        ara = ara % value
+    end
+    return roman
 end
